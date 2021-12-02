0fe252baabe8169001cd8f39e133998c7ed5ee8a
Vladyslav Nedomovnyi <vladyslav@runloop.co>
2021-12-02 19:59:40 +0200

    Implement displaying a progress bar while the loading process

--- app/src/main/java/com/vnedomovnyi/randomusersmvi/ui/user_list/UserListFragment.kt
+++ app/src/main/java/com/vnedomovnyi/randomusersmvi/ui/user_list/UserListFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.vnedomovnyi.randomusersmvi.R
@@ -50,6 +51,9 @@ class UserListFragment : Fragment() {
 
     private fun render(viewState: UserListState) {
         with(viewState) {
+            binding.progressBar.isVisible = viewState.isLoading
+            binding.recyclerView.isVisible = users != null
+
             adapter.submitList(viewState.users ?: emptyList())
 
             error?.consume {
