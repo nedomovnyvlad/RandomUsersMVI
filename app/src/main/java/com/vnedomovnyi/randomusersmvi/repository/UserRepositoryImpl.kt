00ab1cc643339f042c080de711845bc6929675d6
Vladyslav Nedomovnyi <vladyslav@runloop.co>
2021-12-05 14:34:58 +0200

    Fix duplicated results from cache in the UserRepositoryImpl

--- app/src/main/java/com/vnedomovnyi/randomusersmvi/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/vnedomovnyi/randomusersmvi/repository/UserRepositoryImpl.kt
@@ -5,7 +5,6 @@ import com.vnedomovnyi.randomusersmvi.db.UserDao
 import com.vnedomovnyi.randomusersmvi.entity.User
 import com.vnedomovnyi.randomusersmvi.retrofit.UserService
 import com.vnedomovnyi.randomusersmvi.retrofit.response.toUser
-import io.reactivex.rxjava3.core.Maybe
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Single
 
@@ -15,20 +14,22 @@ class UserRepositoryImpl(
 ) : UserRepository {
 
     override fun loadUsers(): Observable<List<User>> {
-        return userDao.subscribe().firstOrError()
+        return loadUsersFromCache()
             .filter { list -> list.isNotEmpty() }
             .switchIfEmpty(loadUsersFromNetwork())
             .toObservable()
-            .mergeWith(userDao.subscribe())
+            .mergeWith(userDao.subscribe().skip(1))
     }
 
-    private fun loadUsersFromNetwork(): Maybe<List<User>> {
+    private fun loadUsersFromNetwork(): Single<List<User>> {
         return Single.fromCallable { userService.getUsers(USER_COUNT).execute() }
             .map { it.body() }
             .map { it!!.users.map { apiUser -> apiUser.toUser() } }
             .flatMapCompletable { userDao.insert(it) }
-            .toMaybe()
+            .andThen(loadUsersFromCache())
     }
+
+    private fun loadUsersFromCache() = userDao.subscribe().firstOrError()
 
     override fun deleteUser(userId: Int) {
         userDao.delete(userId)
